4d2941f91d457e6ea457531f63f7aec040eb25be
hritikdubey-22 <hritik.dubey@polynomial.a>
2024-06-03 22:58:26 +0530

    added second pattern

--- myPattern.ts
+++ myPattern.ts
@@ -1,16 +1,37 @@
-// Input:  ‘N’ = 3
+// // Input:  ‘N’ = 3
 
-// Output: 
-// * 
-// * *
-// * * *
-function printNumber(n:number):void{
-    for(let i:number=1;i<=n;i++){
-        let str:string = ""
-        for (let j: number = 0; j < i; j++) {
-            str+="*"
+// // Output: 
+// // * 
+// // * *
+// // * * *
+// function printNumber(n:number):void{
+//     for(let i:number=1;i<=n;i++){
+//         let str:string = ""
+//         for (let j: number = 0; j < i; j++) {
+//             str+="*"
+//         }
+//         console.log(str)
+//     }
+// }
+// printNumber(3)
+
+
+
+// Input: ‘N’ = 3
+
+// Output:
+// 1
+// 1 2
+// 1 2 3
+
+
+function printPattern(n:number):void{
+    for(let i :number=1;i<=n;i++){
+        let str =  ""
+        for(let j:number=1;j<=i;j++){
+            str+=j;
         }
         console.log(str)
     }
 }
-printNumber(3)
+printPattern(3)
